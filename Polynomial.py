be7ef98e90c2a51d8364d1b38e4bfae97e76bd44
Olga1405 <olka-lyapa@mail.ru>
2018-03-30 21:28:49 +0300

    add test pn*const & test pn+const

--- Polynomial.py
+++ Polynomial.py
@@ -1,13 +1,13 @@
 ﻿class Polynomial(object):
-	def __init__(self, coeffs):
-		for c in coeffs:
-			if not type(c) in [int]:
-				raise Exception("Error! Coefficients must be integers")
-		self.coeffs = coeffs[:]
-		self.__refresh()
+    def __init__(self, coeffs):
+        for c in coeffs:
+            if not type(c) in [int]:
+                raise Exception("Error! Coefficients must be integers")
+        self.coeffs = coeffs[:]
+        self.__refresh()
 
-	def __add__(self, other):
-         if isinstance(other, Polynomial):
+    def __add__(self, other):
+        if isinstance(other, Polynomial):
             res_coeffs = []
             diff = len(self.coeffs) - len(other.coeffs)
             if diff > 0:
@@ -18,78 +18,76 @@
                 res_coeffs = other.coeffs[:]
                 for i in range(0, len(self.coeffs)):
                     res_coeffs[-diff + i] += self.coeffs[i]
-         else:
+        else:
             if self.coeffs:
                 res_coeffs = self.coeffs[:]
                 res_coeffs[-1] += other
             else:
                 res_coeffs = other
-         return Polynomial(res_coeffs)
+        return Polynomial(res_coeffs)
 
-	def __radd__(self, other):
-		return not self + other
+    def __radd__(self, other):
+        return self + other
         
-        def __rmul__(self, other):
-	            return not self * other
+    def __rmul__(self, other):
+        return self * other
 
-	def __mul__(self, other):
-         if isinstance(other, Polynomial):
-		   res_coeffs = (len(self.coeffs) + len(other.coeffs) - 1) * [0]
-		   for i in range(0, len(self.coeffs)):
-			   for j in range(0, len(other.coeffs)):
-				   res_coeffs[i+j] += self.coeffs[i] * other.coeffs[j]
-         else:
+    def __mul__(self, other):
+        if isinstance(other, Polynomial):
+            res_coeffs = (len(self.coeffs) + len(other.coeffs) - 1) * [0]
+            for i in range(0, len(self.coeffs)):
+                for j in range(0, len(other.coeffs)):
+                    res_coeffs[i+j] += self.coeffs[i] * other.coeffs[j]
+        else:
             res_coeffs = [coef*other for coef in self.coeffs]
-         return Polynomial(res_coeffs)
+        return Polynomial(res_coeffs)
 
-	def __eq__(self, other):
-		if len(self.coeffs) != len(other.coeffs):
-			return False
-		for i in range(0, len(self.coeffs)):
-			if self.coeffs[i] != other.coeffs[i]:
-				return False
-		return True
-	
-	def __ne__(self, other):
-		return not self == other
+    def __eq__(self, other):
+        if len(self.coeffs) != len(other.coeffs):
+            return False
+        for i in range(0, len(self.coeffs)):
+             if self.coeffs[i] != other.coeffs[i]:
+                 return False
+        return True
 
-	def __str__(self):
-		res_str = ""
-		power = len(self.coeffs) - 1
-		for c in self.coeffs[:-1]:
-			if c != 0:
-				if res_str != "":
-					if c > 0:
-						res_str += "+"
-				if power != 1:					
-					if c == 1:
-						res_str += 'x%d' % power
-					elif c == -1:
-						res_str += '-x%d' % power
-					else:
-						res_str += '%dx%d' % (c, power)
-				else:
-					if c == 1:
-						res_str += 'x'
-					elif c == -1:
-						res_str += '-x'
-					else: 
-						res_str += '%dx' % c
-			power -= 1		
-		if len(self.coeffs) != 0 and self.coeffs[-1] != 0:
-			if self.coeffs[-1] > 0 and res_str != "":
-				res_str += "+"
-			res_str += str(self.coeffs[-1])
+    def __ne__(self, other):
+        return not self == other
 
-		if res_str == "":
-			res_str = "0"
-		return res_str
+    def __str__(self):
+        res_str = ""
+        power = len(self.coeffs) - 1
+        for c in self.coeffs[:-1]:
+            if c != 0:
+                if res_str != "":
+                    if c > 0:
+                        res_str += "+"
+                if power != 1:					
+                    if c == 1:
+                        res_str += 'x%d' % power
+                    elif c == -1:
+                        res_str += '-x%d' % power
+                    else:
+                        res_str += '%dx%d' % (c, power)
+                else:
+                    if c == 1:
+                        res_str += 'x'
+                    elif c == -1:
+                        res_str += '-x'
+                    else: 
+                        res_str += '%dx' % c
+            power -= 1		
+        if len(self.coeffs) != 0 and self.coeffs[-1] != 0:
+            if self.coeffs[-1] > 0 and res_str != "":
+                res_str += "+"
+            res_str += str(self.coeffs[-1])
 
-	def __refresh(self):
-		if len(self.coeffs) != 0:
-			while self.coeffs[0] == 0 and len(self.coeffs) != 1:
-				del self.coeffs[0]
-		else:
-			self.coeffs.append(0)
+        if res_str == "":
+            res_str = "0"
+        return res_str
 
-
+    def __refresh(self):
+        if len(self.coeffs) != 0:
+            while self.coeffs[0] == 0 and len(self.coeffs) != 1:
+                del self.coeffs[0]
+        else:
+            self.coeffs.append(0)
--- PolynomialTest.py
+++ PolynomialTest.py
@@ -1,51 +1,59 @@
 ﻿import unittest
-from Polynomial import Polynomial
+from polynomial import Polynomial
 
 class PolynomialTest(unittest.TestCase):
-	def test_str_null(self):
-		self.assertEqual(str(Polynomial([0, 0, 0, 0])), '0')
+    def test_str_null(self):
+        self.assertEqual(str(Polynomial([0, 0, 0, 0])), '0')
 
-	def test_str_first_(self):
-		self.assertEqual(str(Polynomial([0, 2])), '2')
+    def test_str_first_(self):
+        self.assertEqual(str(Polynomial([0, 2])), '2')
 
-	def test_str_any_positive_numbers_poly(self):
-		self.assertEqual(str(Polynomial([4,3,2,1])), '4x3+3x2+2x+1')
+    def test_str_any_positive_numbers_poly(self):
+        self.assertEqual(str(Polynomial([4,3,2,1])), '4x3+3x2+2x+1')
+
+    def test_add_zero(self): 
+        self.assertEqual(Polynomial([-4,3,-2,1]), Polynomial([-4,3,-2,1]) + Polynomial([0]))
+
+    def test_add_const(self): 
+        self.assertEqual(Polynomial([-4,3,-2,3]), Polynomial([-4,3,-2,1]) + 2)
+
+    def test_mult_const(self): 
+        self.assertEqual(Polynomial([-4,3,-2,1]), Polynomial([-4,3,-2,1]) * 1)    
+
+    def test_mult_const_left(self): 
+        p = Polynomial([-4,3,-2,1])
+        self.assertListEqual((1 * p ).coeffs, [-4,3,-2,1])  
         
-	def test_add_zero(self): 
-		self.assertEqual(Polynomial([-4,3,-2,1]), Polynomial([-4,3,-2,1]) + Polynomial([0]))
+    def test_add_const_left(self):
+        p = Polynomial([-4,3,-2,1])
+        self.assertListEqual((2 + p).coeffs, [-4,3,-2,3])  
 
-        def test_add_const(self): 
-	    self.assertEqual(Polynomial([-4,3,-2,3]), Polynomial([-4,3,-2,1]) + 2)
+    def test_add_number(self):
+        self.assertEqual(Polynomial([4,3,2,2]), Polynomial([4,3,2,1]) + Polynomial([1]))
 
-        def test_mult_const(self): 
-	    self.assertEqual(Polynomial([-4,3,-2,1]), Polynomial([-4,3,-2,1]) * 1)
+    def test_add_negative_number(self):
+        self.assertEqual(Polynomial([-4,3,-2,0]), Polynomial([-4,3,-2,1]) + Polynomial([-1]))
 
-	def test_add_number(self):
-		self.assertEqual(Polynomial([4,3,2,2]), Polynomial([4,3,2,1]) + Polynomial([1]))
+    def test_add_polynom_same_size(self):
+        self.assertEqual(Polynomial([-8,6,-4,0]), Polynomial([-4,3,-2,0]) + Polynomial([-4,3,-2,0]))
 
-	def test_add_negative_number(self):
-		self.assertEqual(Polynomial([-4,3,-2,0]), Polynomial([-4,3,-2,1]) + Polynomial([-1]))
+    def test_add_polynom_less_size(self):
+        self.assertEqual(Polynomial([-1,2,-2,-5]), Polynomial([-1,3,-5,0]) + Polynomial([-1,3,-5]))
 
-	def test_add_polynom_same_size(self):
-		self.assertEqual(Polynomial([-8,6,-4,0]), Polynomial([-4,3,-2,0]) + Polynomial([-4,3,-2,0]))
+    def test_add_polynom_bigger_size(self):
+        self.assertEqual(Polynomial([-1,2,-2,-5]), Polynomial([-1,3,-5]) + Polynomial([-1,3,-5,0]))
 
-	def test_add_polynom_less_size(self):
-		self.assertEqual(Polynomial([-1,2,-2,-5]), Polynomial([-1,3,-5,0]) + Polynomial([-1,3,-5]))
+    def test_mul_zero(self):
+        self.assertEqual(Polynomial([0]), Polynomial([-1,3,-5,0]) * Polynomial([0]))
 
-	def test_add_polynom_bigger_size(self):
-		self.assertEqual(Polynomial([-1,2,-2,-5]), Polynomial([-1,3,-5]) + Polynomial([-1,3,-5,0]))
+    def test_mul_positive_number(self):
+        self.assertEqual(Polynomial([-2,6,-10,2]), Polynomial([-1,3,-5,1]) * Polynomial([2]))
 
-	def test_mul_zero(self):
-		self.assertEqual(Polynomial([0]), Polynomial([-1,3,-5,0]) * Polynomial([0]))
+    def test_mul_negative_number(self):
+        self.assertEqual(Polynomial([2,-6,10,-2]), Polynomial([-1,3,-5,1]) * Polynomial([-2]))
 
-	def test_mul_positive_number(self):
-		self.assertEqual(Polynomial([-2,6,-10,2]), Polynomial([-1,3,-5,1]) * Polynomial([2]))
-
-	def test_mul_negative_number(self):
-		self.assertEqual(Polynomial([2,-6,10,-2]), Polynomial([-1,3,-5,1]) * Polynomial([-2]))
-	
-	def test_mul_polynoms(self):
-		self.assertEqual(Polynomial([-2,3,-1,-13,3]), Polynomial([-1,3,-5,1]) * Polynomial([2,3]))
+    def test_mul_polynoms(self):
+        self.assertEqual(Polynomial([-2,3,-1,-13,3]), Polynomial([-1,3,-5,1]) * Polynomial([2,3]))
 
 if __name__ == '__main__':
     unittest.main()
